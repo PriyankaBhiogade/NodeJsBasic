e2090e9e79bff1e9732f47122987ed0fbc1150ac
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-26 10:33:13 +0530

    Added: Code To conversion of FeetToInch and FeetToMeter

---
+++ unitConversion.js
@@ -0,0 +1,14 @@
+let FEET = 1;
+let INCH = 12;
+let givenInchVal =42;
+let feetToInch = FEET * INCH;
+console.log("42 in = ",givenInchVal / feetToInch ,"ft");
+
+let width = 60 ;
+let length = 40 ;
+let oneFeetToMeter = 0.3048 ;
+let areaInFeet = width * length ;
+console.log("Area of Rectangle in feet : ",areaInFeet);
+console.log("Area of Rectangle in Meter : "
+,Math.floor(areaInFeet / oneFeetToMeter)); 
+
