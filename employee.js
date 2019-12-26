828be4cc30385bb5eab5143124a0a28eeee82128
PriyanakaBhioade <bhiogadePriyanak93@gmail.com>
2019-12-26 23:43:25 +0530

    Added :Code To find Wage(WhileLoop)

--- employee.js
+++ employee.js
@@ -1,25 +1,29 @@
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
+let MAX_HRS_IN_MONTH = 10;
 let EMP_RATE_PER_HOUR = 20;
-let NUM_OF_WORKING_DAYS = 2;
+let NUM_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
-let empWage = 0;
+let totalEmpHrs = 0;
 let totalWage = 0;
+let totalWorkingDays = 0;
 
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){ 
-	let empCheck = Math.floor(Math.random() * 10) % 3;
-	switch (empCheck) {
-	case IS_PART_TIME:
-		empHrs = 4;
-		break;
-	case IS_FULL_TIME:
-        	empHrs = 8;
-		break;
-	default:
-		empHrs = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+	totalWorkingDays <= NUM_OF_WORKING_DAYS){
+		totalWorkingDays++;
+		let empCheck = Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+        		empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+		totalEmpHrs += empHrs ;
 	}
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	totalWage += empWage;
-}
-
+totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log(totalEmpWage); 
