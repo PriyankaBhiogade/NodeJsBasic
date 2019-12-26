a9fea7e4f1ba2cb6b383d17969986812b069ae6f
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-26 16:00:54 +0530

    Added: Code To Check Part Time Wage

--- employee.js
+++ employee.js
@@ -1,11 +1,15 @@
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
+
 let empHrs = 0;
 let empWage = 0;
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 2);
-if(empCheck  == isPresent)
-	empHrs = 8;
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+if(empCheck  == IS_PART_TIME)
+	empHrs = 4;
+else if(empCheck  == IS_FULL_TIME)
+        empHrs = 8;
 else
 	empHrs = 0;
 empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Daily Employee Wage:",empWage);
