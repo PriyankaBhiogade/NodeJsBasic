043625d6f9e46956368cb8253be449638881ba09
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-26 10:54:40 +0530

    Refactor:Code Remove Printing Statement

--- sumAvg.js
+++ sumAvg.js
@@ -3,8 +3,8 @@ let sum = 0;
 
 for(let i = 0; i<5; i++){
 	let randomNum = Math.floor(Math.random() * 90 + 10);
-		sum = sum + randomNum;
-		count++;
+	sum = sum + randomNum;
+	count++;
 }
 
 let avg = sum / count;
