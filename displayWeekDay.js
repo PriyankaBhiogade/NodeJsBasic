41a3c5025d5502b2b3536b61ef1f190104a81357
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-26 17:45:17 +0530

    Refactor:Code Used Switch Case Instead of if loop

--- displayWeekDay.js
+++ displayWeekDay.js
@@ -1,21 +1,29 @@
 let read =require ('readline-sync');
-let num = read.question("Enter Number: ");
+let num = read.questionInt("Enter Number: ");
 let day;
-if(num == 1)
+switch(num) {
+case 1:
         day ="Monday";
-else if(num == 2)
+	break;
+case 2:
         day ="Tuesday";
-else if(num == 3)
+	break;
+case 3:
         day ="Wednesday";
-else if(num == 4)
+	break;
+case 4:
         day ="Thursday";
-else if(num == 5)
+	break;
+case 5:
         day ="Firday";
-else if(num == 6)
+	break;
+case 6:
         day ="Saturday";
-else if(num == 7)
+	break;
+case 7:
         day ="Sunday";
-else
+	break;
+default:
 	console.log("Invalid Number");
-
+}
 console.log(num,":",day);
