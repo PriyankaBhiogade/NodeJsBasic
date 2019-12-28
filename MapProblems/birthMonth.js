d0b34a96a4e206124e335351f42a18fe769658a0
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-28 13:12:01 +0530

    Added:Code to find all individuals who has birthday in same month

--- MapProblems/birthMonth.js
+++ MapProblems/birthMonth.js
@@ -1,14 +1,13 @@
-let months = ["0","Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
+let months = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
 let personMap = new Map();
 let personList = [];
 
-for(let i = 1;i <= 12; i++)
-{
-	personList=[i] = new Map();
+for(let i = 1; i <= 12; i++){
+	personList[i] = new Map();
 }
 
-for(let i = 1; i <= 50; i++){
-	month = Math.floor(Math.random()*10)%12+1;
+for(let i = 1;i <= 50; i++){
+	month = Math.floor(Math.random()* 10) % 12 + 1;
 	personMap.set(months[month],personList[month].set(i,"person "+i));
 }
 
